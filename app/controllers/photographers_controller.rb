73b8d30f3fbcef4b154efbdcb66f4a8f09b59ded
Evan Byrd <elbyrd09@gmail.com>
2020-03-11 17:40:00 +0800

    updating from last branch

--- app/controllers/photographers_controller.rb
+++ app/controllers/photographers_controller.rb
@@ -1,8 +1,12 @@
 class PhotographersController < ApplicationController
   def new
+    @photographer = Photographer.new
   end
 
   def create
+    @photographer = Photographer.new(photographer_params)
+    @photographer.user = current_user
+
   end
 
   def edit
@@ -13,4 +17,11 @@ class PhotographersController < ApplicationController
 
   def destroy
   end
+
+  private
+
+  def photographer_params
+    params.require(:photographer).permit(:camera, :lenses, :profession, :phone_number)
+  end
+
 end
