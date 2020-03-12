f216011dd629988f5ac5558b707b3b9ec7e1f1a2
jawad sarfraz <jadiman@gmail.com>
2020-03-12 14:19:17 +0700

    udpated pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,10 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  # skip_before_action :authenticate_user!, only: [:home]
   def home
     @events = Event.all
+  end
+
+  def dashboard
+    @user = current_user
   end
 end
