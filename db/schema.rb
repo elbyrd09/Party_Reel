27a7a79cd5bee91d0b267512bf377b1e6d329811
Matthijs van Oostende <matthijsvanoostende@hotmail.com>
2020-03-13 11:35:37 +0800

    prices added, with money-rails gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_12_073233) do
+ActiveRecord::Schema.define(version: 2020_03_13_032247) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -94,11 +94,11 @@ ActiveRecord::Schema.define(version: 2020_03_12_073233) do
 
   create_table "packages", force: :cascade do |t|
     t.string "session"
-    t.integer "price"
     t.text "description"
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "price_cents", default: 0, null: false
   end
 
   create_table "photographers", force: :cascade do |t|
