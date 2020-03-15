8cc302ca2f888cd91f5a2f56a037a5476a56ab5d
Matthijs van Oostende <matthijsvanoostende@hotmail.com>
2020-03-15 12:39:48 +0800

    added showbottomNav method

---
+++ app/javascript/packs/show_bottom_nav.js
@@ -0,0 +1,15 @@
+const showBottomNav = () => {
+  document.querySelector('#starttime').addEventListener('change', (e)=>{
+    let starttime = e.target.value
+    document.querySelector('#booking_start_time').value = starttime
+    document.querySelector('#quote-start-time').innerText = starttime
+  })
+  document.querySelector('#endtime').addEventListener('change', (e)=>{
+    let endtime = e.target.value
+    document.querySelector('#booking_end_time').value = endtime
+    document.querySelector('#quote-end-time').innerText = endtime
+  })
+
+}
+
+export { getBookingTime };
