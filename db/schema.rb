6d83525021c2296c005dc8538600ae0a76255d0d
Evan Byrd <elbyrd09@gmail.com>
2020-03-18 15:51:58 +0800

    fbook start

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_15_074341) do
+ActiveRecord::Schema.define(version: 2020_03_18_074934) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -164,6 +164,8 @@ ActiveRecord::Schema.define(version: 2020_03_15_074341) do
     t.string "last_name"
     t.string "location"
     t.string "user_ig_handle"
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
