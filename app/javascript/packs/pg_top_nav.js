eee1dd1d74065b372c7a8ee8fcbc81d60cdb88df
Evan Byrd <elbyrd09@gmail.com>
2020-04-09 12:48:59 -0400

    scroll fixed and bottom nav z-index adjusted so content does not appear through bottom nav

--- app/javascript/packs/pg_top_nav.js
+++ app/javascript/packs/pg_top_nav.js
@@ -1,10 +1,14 @@
 const navOnScroll = () => {
   const navbar = document.querySelector('.custom-nav');
+  const userScroll = window.innerHeight * .69
+  // multiplies innerHeight of viewport by 69% which is the mark that the user scrolls to
+  // on the homepage after the banner image has been passed, which, we then add the 'nav-custom-disbale'
+  // class to the navbar which hides and disables it's options, ie. About Us, FAQ, Join the party
 
   if(navbar) {
     window.addEventListener('scroll', () => {
       console.log(window.scrollY + ',' + window.innerHeight)
-      if (window.scrollY >= window.innerHeight) {
+      if (window.scrollY >= userScroll) {
         navbar.classList.add('nav-custom-disable');
       } else {
         navbar.classList.remove('nav-custom-disable');
--- app/javascript/plugins/swipable.js
+++ app/javascript/plugins/swipable.js
@@ -1,110 +1,110 @@
-function resetCarousel() {
-  const first = document.querySelector('#first')
-  const second = document.querySelector('#second')
-  const third = document.querySelector('#third')
-  const fourth = document.querySelector('#fourth')
-  const fifth = document.querySelector('#fifth')
-  const sixth = document.querySelector('#sixth')
-  const seventh = document.querySelector('#seventh')
+// function resetCarousel() {
+//   const first = document.querySelector('#first')
+//   const second = document.querySelector('#second')
+//   const third = document.querySelector('#third')
+//   const fourth = document.querySelector('#fourth')
+//   const fifth = document.querySelector('#fifth')
+//   const sixth = document.querySelector('#sixth')
+//   const seventh = document.querySelector('#seventh')
 
-  const nodes = [first, second, third, fourth, fifth, sixth, seventh]
+//   const nodes = [first, second, third, fourth, fifth, sixth, seventh]
 
-  nodes.forEach(node => {
-    removeClass(node)
-  })
+//   nodes.forEach(node => {
+//     removeClass(node)
+//   })
 
-  first.classList.add('hideLeft')
-  second.classList.add('prevLeftSecond')
-  third.classList.add('prev')
-  fourth.classList.add('selected')
-  fifth.classList.add('next')
-  sixth.classList.add('nextRightSecond')
-  seventh.classList.add('hideRight')
-}
+//   first.classList.add('hideLeft')
+//   second.classList.add('prevLeftSecond')
+//   third.classList.add('prev')
+//   fourth.classList.add('selected')
+//   fifth.classList.add('next')
+//   sixth.classList.add('nextRightSecond')
+//   seventh.classList.add('hideRight')
+// }
 
-function removeClass(node) {
-  node.className = '';
-}
+// function removeClass(node) {
+//   node.className = '';
+// }
 
-export function moveToSelected(element) {
-  if (element == "next") {
-    var selected = $(".selected").next();
-  } else if (element == "prev") {
-    var selected = $(".selected").prev();
-  } else {
-    var selected = element;
-  }
+// export function moveToSelected(element) {
+//   if (element == "next") {
+//     var selected = $(".selected").next();
+//   } else if (element == "prev") {
+//     var selected = $(".selected").prev();
+//   } else {
+//     var selected = element;
+//   }
 
-  var next = $(selected).next();
-  var prev = $(selected).prev();
-  var prevSecond = $(prev).prev();
-  var nextSecond = $(next).next();
+//   var next = $(selected).next();
+//   var prev = $(selected).prev();
+//   var prevSecond = $(prev).prev();
+//   var nextSecond = $(next).next();
 
-  $(selected)
-    .removeClass()
-    .addClass("selected");
+//   $(selected)
+//     .removeClass()
+//     .addClass("selected");
 
-  $(prev)
-    .removeClass()
-    .addClass("prev");
+//   $(prev)
+//     .removeClass()
+//     .addClass("prev");
 
-  $(next)
-    .removeClass()
-    .addClass("next");
+//   $(next)
+//     .removeClass()
+//     .addClass("next");
 
-  $(nextSecond)
-    .removeClass()
-    .addClass("nextRightSecond");
+//   $(nextSecond)
+//     .removeClass()
+//     .addClass("nextRightSecond");
 
-  $(prevSecond)
-    .removeClass()
-    .addClass("prevLeftSecond");
+//   $(prevSecond)
+//     .removeClass()
+//     .addClass("prevLeftSecond");
 
-  $(nextSecond)
-    .nextAll()
-    .removeClass()
-    .addClass("hideRight");
+//   $(nextSecond)
+//     .nextAll()
+//     .removeClass()
+//     .addClass("hideRight");
 
-  $(prevSecond)
-    .prevAll()
-    .removeClass()
-    .addClass("hideLeft");
-}
+//   $(prevSecond)
+//     .prevAll()
+//     .removeClass()
+//     .addClass("hideLeft");
+// }
 
-// Keyboard events
-$(document).keydown(function(e) {
-  switch (e.which) {
-    case 37: // left
-      moveToSelected("prev");
-      break;
+// // Keyboard events
+// $(document).keydown(function(e) {
+//   switch (e.which) {
+//     case 37: // left
+//       moveToSelected("prev");
+//       break;
 
-    case 39: // right
-      moveToSelected("next");
-      break;
+//     case 39: // right
+//       moveToSelected("next");
+//       break;
 
-    default:
-      return;
-  }
-  e.preventDefault();
-});
+//     default:
+//       return;
+//   }
+//   e.preventDefault();
+// });
 
-$("#carousel div").click(function() {
-  moveToSelected($(this));
-});
+// $("#carousel div").click(function() {
+//   moveToSelected($(this));
+// });
 
-$("#prev").click(function() {
-  moveToSelected("prev");
-});
+// $("#prev").click(function() {
+//   moveToSelected("prev");
+// });
 
-$("#next").click(function() {
-  moveToSelected("next");
-});
+// $("#next").click(function() {
+//   moveToSelected("next");
+// });
 
-setInterval(() => {
-  const seventh = document.querySelector('#seventh')
-  if (seventh && seventh.classList.contains('selected')) {
-    resetCarousel()
-  } else {
-    moveToSelected("next");
-  }
-}, 2000)
+// setInterval(() => {
+//   const seventh = document.querySelector('#seventh')
+//   if (seventh && seventh.classList.contains('selected')) {
+//     resetCarousel()
+//   } else {
+//     moveToSelected("next");
+//   }
+// }, 2000)
