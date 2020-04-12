2c52b7ca447396b9f235f36522b01c6997baa8a5
Matthijs van Oostende <matthijsvanoostende@hotmail.com>
2020-04-12 17:30:00 +0200

    added if statement to prevent bottom_nav = null error message

--- app/javascript/packs/show_bottom_nav.js
+++ app/javascript/packs/show_bottom_nav.js
@@ -1,8 +1,11 @@
 const showBottomNav = () => {
-  document.querySelector('.bottom-nav-avatar').addEventListener('click', (e)=>{
-  document.querySelectorAll('.bottom-nav-icon').forEach (icon =>
-    icon.classList.toggle("bottom-nav-icon-show"));
-  })
+  let bottom_nav = document.querySelector('.bottom-nav-avatar');
+  if (bottom_nav) {
+    bottom_nav.addEventListener('click', (e)=>{
+      document.querySelectorAll('.bottom-nav-icon').forEach (icon =>
+      icon.classList.toggle("bottom-nav-icon-show"));
+    })
+  }
 }
 
 export { showBottomNav };
