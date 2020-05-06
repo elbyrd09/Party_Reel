6c67796f3bbb78b7c99fb3fd34dee11d37368049
Yassin Elsir <yassinmelsir@gmail.com>
2020-05-06 14:36:21 +0800

    updated dashboard view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,7 +11,7 @@ class PagesController < ApplicationController
   def dashboard
     @user = current_user
     # if current_user.is_photographer
-    #@events = Event.all
+    @events = Event.all
     @bookings = Booking.where(photographer_id: current_user.photographer)
     # add the current_user to find the unique bookings for a photographer's dashboard
     # else
