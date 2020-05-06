64a5abd2bd8d319ee9fdc4cceffc54dd555f956c
Yassin Elsir <yassinmelsir@gmail.com>
2020-05-06 16:12:36 +0800

    created flip event info function for dashboard

--- app/javascript/packs/flip_package_info.js
+++ app/javascript/packs/flip_package_info.js
@@ -3,7 +3,7 @@ const flipPackageInfo = () => {
   document.querySelectorAll('.package-card').forEach (card =>
       card.querySelectorAll('.package-toggle').forEach (title =>
       title.addEventListener('click', (e)=>{
-      card.childNodes[1].classList.toggle('hidden');
+
       card.childNodes[3].classList.toggle('hidden');
     }
   )))
